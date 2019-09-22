32f4351c05be4278215fffaca5241b046f98fad7
John Atti <thejohnatti@gmail.com>
2019-09-22 19:35:22 -0400

    Initial Commit

---
+++ TheFiveCrowns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TheFiveCrowns
+//
+//  Created by John Atti on 9/22/19.
+//  Copyright © 2019 John Atti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
