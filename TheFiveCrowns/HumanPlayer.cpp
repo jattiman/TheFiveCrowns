71105f9c280ee0b28d70b22f849240a70fadd35b
John Atti <thejohnatti@gmail.com>
2019-10-01 22:56:10 -0400

    Added to playRound function.

--- TheFiveCrowns/HumanPlayer.cpp
+++ TheFiveCrowns/HumanPlayer.cpp
@@ -27,20 +27,26 @@ void HumanPlayer::sayIfHuman(){
 
 void HumanPlayer::playRound(Deck *deck){
     int userChoice=0;
-    cout << "What do you want to do?" << endl;
+    // give player option to draw card
+    // move this to a new function -------
     do{
         cout << "Where would you like to draw from?" << endl
         << "\t1. The draw pile" << endl
         << "\t2. The discard pile" << endl;
         cin >> userChoice;
     } while(userChoice < 1 || userChoice > 2);
+    // transfer the card appropriately
     if(userChoice==1){
         deck->transferCard(deck->getDrawPile(), deck->getHumanDeck());
     }
     else{
+        // need to check to ensure piles aren't empty
         deck->transferFromDiscard(deck->getHumanDeck());
     }
-    
+    // if player has wild card, ask them if they want to rename any
+    // this->reviewWilds(), maybe?
+    // ask player which card they would like to discard
+    // this->choose discard
     return;
 }
 
