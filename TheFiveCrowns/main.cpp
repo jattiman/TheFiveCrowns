db337437d1f54a00c9594e8e3f8a0e0687a21138
John Atti <thejohnatti@gmail.com>
2019-10-23 15:28:06 -0400

    Removed excess code/comments from debug.

--- TheFiveCrowns/main.cpp
+++ TheFiveCrowns/main.cpp
@@ -26,20 +26,6 @@ int main(int argc, const char * argv[]) {
     
     Game ourGame;
     ourGame.welcome();
-    
-//    Deck ourDeck(1);
-//    ourDeck.printDecks();
-//    ourDeck.testDeck();
-    
-//    cout << "Human: ";
-//    ourDeck.printTheDeck(ourDeck.getHumanDeck());
-//    cout << "Computer: ";
-//    ourDeck.printTheDeck(ourDeck.getComputerDeck());
-//    ourDeck.testDeck();
-//    Card newCard("3H",1);
-//    cout << "Getting card string: " << endl;
-//    cout << newCard.getCardString();
 
-//    cout << "Back in main, now." << endl;   // debug
     return 0;
 }
